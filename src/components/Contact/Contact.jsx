7f0e3699b00f0fc9f67ff19bcda13a543111e359
Tayfun <me@tayfun.dev>
2025-08-06 22:37:38 +0300

    ....

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../store/contactsSlice'; // action'ı buradan çekiyoruz
 import styles from './Contact.module.css';
 
-export default function Contact({ contact, onDelete }) {
+export default function Contact({ contact }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(contact.id));
+  };
+
   return (
     <div className={styles.contact}>
       <p>👤 {contact.name}</p>
       <p>📞 {contact.number}</p>
-      <button onClick={() => onDelete(contact.id)}>Delete</button>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
